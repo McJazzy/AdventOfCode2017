d33f4187f181ac8e28a620ab215327da09023cd1
Michiel <michieljaspers90@gmail.com>
2017-12-05 23:01:52 +0100

    faster day5

--- AdventOfCode2k17/Day5.cpp
+++ AdventOfCode2k17/Day5.cpp
@@ -8,15 +8,15 @@
 #include <map>
 #include <complex>
 #include <set>
-
+#include <chrono>
 template<>
 void solve<Day5>(std::istream & is, std::ostream & os, bool part2) {
 	std::vector<int> v{ std::istream_iterator<int>{is},{} };	
-	int steps{ 0 };
-	for(int idx=0;idx >= 0 && idx < v.size(); ++steps) {
-		int new_idx = idx + v[idx];		
-		v[idx] += part2 && v[idx] >= 3 ? -1 : 1;
-		idx = new_idx;
-	}
+	int steps{ 0 };	
+	for (auto it{ v.begin() }; it >= v.begin() && it < v.end(); ++steps) {
+		auto old = it;
+		it += *it;
+		*old += part2 && *old >= 3 ? -1 : 1;		
+	}	
 	os << "part" << (part2 ? "2" : "1") << " =" << steps << " ";
 }
